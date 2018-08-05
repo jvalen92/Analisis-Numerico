0a4f168303e519c1361e9b22cc40d026280bc9c9
angrajales <38627313+angrajales@users.noreply.github.com>
2018-08-05 17:37:47 -0500

    Añadiendo el punto 4

--- tarea2/tarea2.py
+++ tarea2/tarea2.py
@@ -16,7 +16,58 @@ def taylorExp0(x):
     for n in range(0,40):
         result += pow(x,n)/math.factorial(n)
     print(result)
+# Punto 4(Primera expresion)
+def cosPlusExp0(x):
+    resultado1 = 1
+    resultado2 = 1
+    error1 = 1
+    error2 = 1
+    n = 1
+    f = open("punto4_expr1.txt", "w")
+    f.write("Funcion cos(x) + exp(x) para x = ")
+    f.write(str(x))
+    f.write('\n')
+    while error1 > 0.5 * 10 ** -18 and error2 > 0.5 * 10 ** -18 and n <= 1000:
+        error1 = (((-1) ** n) * x ** (2 * n)) / math.factorial(2 * n)
+        error2 = (x ** n) / math.factorial(n)
+        resultado1 += error1
+        resultado2 += error2
+        f.write("En la iteracion: %d, resultado: " % n)
+        f.write(str(resultado1 + resultado2))
+        f.write('\n')
+        error1 = abs(error1)
+        error2 = abs(error2)
+        n = n + 1
+    f.write("Resultado final: ")
+    f.write(str(resultado1 + resultado2))
+    f.close()
 
+
+# Punto 4(Segunda expresion)
+def cosPlusSen0(x):
+    resultado1 = 1
+    resultado2 = x
+    error1 = 1
+    error2 = 1
+    n = 1
+    f = open("punto4_expr2.txt", "w")
+    f.write("Funcion cos(x) + sen(x) para x = ")
+    f.write(str(x))
+    f.write('\n')
+    while error1 > 0.5 * 10 ** -18 and error2 > 0.5 * 10 ** -18 and n <= 1000:
+        error1 = (((-1) ** n) * x ** (2 * n)) / math.factorial(2 * n)
+        error2 = (((-1) ** n) * x ** (2 * n + 1)) / math.factorial(2 * n + 1)
+        resultado1 += error1
+        resultado2 += error2
+        f.write("En la iteracion: %d, resultado: " % n)
+        f.write(str(resultado1 + resultado2))
+        f.write('\n')
+        error1 = abs(error1)
+        error2 = abs(error2)
+        n = n + 1
+    f.write("Resultado final: ")
+    f.write(str(resultado1 + resultado2))
+    f.close()
 def expDiferentesCentors(x):
     
     centro1 = 0
@@ -50,6 +101,12 @@ if __name__ == "__main__":
     print(math.sin(valor))
     taylorExp0(valor)
     print(math.exp(valor))"""
+
+    # Punto 4
+    cosPlusExp0(0.4)
+    cosPlusSen0(0.4)
+
+
     expDiferentesCentors(0.01)
     expDiferentesCentors(math.pi/3)
     expDiferentesCentors(5.4)
