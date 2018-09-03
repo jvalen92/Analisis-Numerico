b3cb6990f98251b3bc6d7e5f526244cd0af798b1
jsanch89 <futbol982009@hotmail.es>
2018-09-03 01:13:20 -0500

    punto fijo

---
+++ SolucionEcuancionesUnaVariable/PuntoFijo.py
@@ -0,0 +1,30 @@
+import sympy
+from prettytable import PrettyTable
+sympy.init_printing(use_unicode=True)
+
+def puntoFijo(tolerancia,xa,niter):
+    table = PrettyTable(['Iteracion', 'Xn', 'f(x)', 'Error'])
+    x = sympy.symbols('x')
+    f = sympy.exp((3*x) - 12) + x * sympy.cos(3 * x) - (x ** 2) + 4 
+    g = -sympy.sqrt(x*sympy.exp(x)-5*x-3)
+    fx = f.evalf(subs={x:xa})
+    contador = 0
+    error = tolerancia + 1 
+    table.add_row([contador, xa, fx, 'no existe'])
+    while fx != 0 and error > tolerancia and contador < niter:
+        xn = g.evalf(subs={x:xa})
+        fx = f.evalf(subs={x:xn})
+        error = abs(xn-xa)
+        xa = xn 
+        contador = contador  + 1
+        table.add_row([contador, xn, fx, error])
+    
+    if fx == 0:
+        print(xa," Es raiz")
+    elif error < tolerancia:
+        print(xa,"Es una aproximacion con tolerancia menor a ",tolerancia)
+    else:
+        print("El metodo fracaso con ",niter," iteraciones")
+    print(table)
+
+puntoFijo(0.0005,-4,30)
