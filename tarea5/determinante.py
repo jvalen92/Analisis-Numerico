b1a958ba388b53d344c86f0fbf6eded8af395057
angrajales <38627313+angrajales@users.noreply.github.com>
2018-09-13 18:31:54 -0500

    Organizando el código y la documentación

--- tarea5/determinante.py
+++ tarea5/determinante.py
@@ -1,10 +1,11 @@
 def det(A, n):
-    if n == 2:
-        return (A[0][0]*A[1][1])-(A[0][1] * A[1][0])
+    if n == 1:
+        return A[0][0]
     resultado = 0
-    for x in range(0, len(A)):
+    for x in range(0, n):
         resultado += power(-1, x) * A[0][x] * det(nueva_matriz(A, x), n - 1)
     return resultado
+
 
 def nueva_matriz(A, i):
     Ap = []
@@ -15,6 +16,8 @@ def nueva_matriz(A, i):
                 v.append(A[x][y])
         Ap.append(v)
     return Ap
+
+
 def power(a, b):
     res = 1
     while(b > 0):
@@ -24,5 +27,8 @@ def power(a, b):
         b >>= 1
     return res
 
+
+"""
 A = [[4, 2, 3, -4], [3, -2, 1, 5], [-2, 0, 1, -3], [8, -2, 6, 4]]
 print(det(A, len(A)))
+"""
