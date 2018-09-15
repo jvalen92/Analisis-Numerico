9f0f702b67dde4b6ac20a4b93b8ea2350d1f2b44
jsanch89 <futbol982009@hotmail.es>
2018-09-15 03:32:05 -0500

    Escalonamiento y despeje

---
+++ tarea5/EliminacionGaussianaSimple.py
@@ -0,0 +1,29 @@
+import Escalonamiento 
+import numpy as np
+
+def despeje(A,b):
+    (A,b) = Escalonamiento.escalonamiento(A,b) #Escalonar A
+    n = len(A)-1
+    x = np.zeros(n+1,float)
+    x[n] = b[n]/A[n][n]
+    for i in range(n-1,-1,-1):
+        sumatoria = 0
+        for p in range(i+1,n+1):
+            sumatoria = sumatoria + A[i][p]*x[p]
+        x[i] = (b[i] - sumatoria)/A[i][i]
+    return x
+
+"""n = int(input(" Ingrese el tamaño deseado: "))
+A = np.zeros((n,n),np.float64)
+b = np.zeros(n,np.float64)
+
+for i in range(n):
+    for j in range(n):
+        A[i][j] = np.float64(input("Ingrese coeficiente de la matriz A en la fila "+str(i+1)+" y columna "+str(j+1)+": ")) 
+
+for i in range(n):
+    b[i]= np.float64(input("Ingrese el valor del termino independiente en la casilla "+str(i+1)+": "))     
+"""
+A = [[14,6,-2,3],[3,15,2,-5],[-7,4,-23,2],[1,-3,-2,16]]
+b = [12,32,-24,14]
+print(despeje(A,b))    
