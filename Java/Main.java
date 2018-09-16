71853139b8c31d7344cfa33db3f7f0b5b3b50c8e
angrajales <38627313+angrajales@users.noreply.github.com>
2018-09-16 17:18:16 -0500

    Creando codigo de ecuaciones lineales

--- Java/Main.java
+++ Java/Main.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
     private static final int N = 100;
     private static final double TOL = 1e-7;
@@ -12,7 +15,29 @@ public class Main {
         UnaVariable.raicesMultiples(1, TOL, N);
         OptimizacionUnaVariable.steffensen(-0.5, N, TOL);
         OptimizacionUnaVariable.muller(0.5, 1, 1.5, TOL, N);
-        SistemaDeEcuaciones.det(new double[][]{{2, -1, 0, 3}, {1, 0.5, 3, 8}, {0, 13, -2, 11}, {14, 5, -2, 3}});
+        double[][] A = new double[][]{{2, -1, 0, 3}, {1, 0.5, 3, 8}, {0, 13, -2, 11}, {14, 5, -2, 3}};
+        double[] b = new double[]{1,1,1,1};
+        double[] x = SistemaDeEcuaciones.despejar(A, b);
+        PrintWriter writer = null;
+        try{
+            writer = new PrintWriter("ecuaciones_lineales.txt");
+        }catch (IOException ex){
+            System.out.println(ex);
+        }
+        writer.println("Det = " + SistemaDeEcuaciones.det(A));
+        writer.println("Solucion: ");
+        writer.print("x = [");
+        for(double r: x) writer.print(" " + r);
+        writer.println(" ]");
+        writer.println("Matriz solucion: ");
+        for(double[] d: A){
+            for(double r: d){
+                writer.printf("%.15f ", r);
+            }
+            writer.println();
+        }
+        writer.flush();
+        writer.close();
         Graficador.graficar();
     }
 }
--- Java/SistemaDeEcuaciones.java
+++ Java/SistemaDeEcuaciones.java
@@ -1,22 +1,23 @@
 public class SistemaDeEcuaciones {
-    private static double det(double[][]A, int n){
-        if(n == 1){
+    private static double det(double[][] A, int n) {
+        if (n == 1) {
             return A[0][0];
         }
         double resultado = 0.0;
-        for(int i = 0; i < A.length; ++i){
-            return ((i & 1) == 0 ? 1: -1) * A[0][i] * det(nuevaMatriz(A, i), n - 1);
+        for (int i = 0; i < A.length; ++i) {
+            return ((i & 1) == 0 ? 1 : -1) * A[0][i] * det(nuevaMatriz(A, i), n - 1);
         }
         return resultado;
     }
-    private static double[][] nuevaMatriz(double[][] A, int x){
+
+    private static double[][] nuevaMatriz(double[][] A, int x) {
         assert A.length > 0 && A[0].length > 0;
-        double[][] newA = new double[A.length-1][A.length - 1];
-        for(int i = 1; i < A.length; ++i) {
+        double[][] newA = new double[A.length - 1][A.length - 1];
+        for (int i = 1; i < A.length; ++i) {
             double[] v = new double[A.length - 1];
             int k = 0;
             for (int j = 0; j < A[0].length; j++) {
-                if(j != x){
+                if (j != x) {
                     v[k++] = A[i][j];
                 }
             }
@@ -24,8 +25,62 @@ public class SistemaDeEcuaciones {
         }
         return newA;
     }
-    public static void det(double[][] A){
+
+    public static double det(double[][] A) {
         assert A.length > 0 && A[0].length > 0 && A.length == A[0].length;
-        System.out.printf("Det = %.15f\n", det(A, A.length));
+        //System.out.printf("Det = %.15f\n", det(A, A.length));
+        return det(A, A.length);
+    }
+
+    public static double[] despejar(double[][] A, double[] b) {
+        Utilities.Par<double[][], double[]> Ab= escalonamiento(A, b);
+        A = Ab.getPrimero();
+        b = Ab.getSegundo();
+        int n = A.length - 1;
+        double[] x = new double[n + 1];
+        x[n] = b[n] / A[n][n];
+        //System.out.println(b[n] + " " + A[n][n] + " " + x[n]);
+        for(int dx = n - 1; dx > -1; --dx){
+            double suma = 0;
+            for(int dy = dx + 1; dy < n + 1; ++dy){
+                suma += A[dx][dy] * x[dy];
+            }
+            x[dx] = (b[dx] - suma) / A[dx][dx];
+        }
+        return x;
+    }
+
+    public static Utilities.Par<double[][], double[]> escalonamiento(double[][] A, double[] b) {
+        int n = A.length;
+        if (n != A[0].length) {
+            System.out.println("La matriz no es adecuada");
+        } else if (b.length != n) {
+            System.out.println("Hay diferente numero de terminos independientes e incognitas");
+        } else if (det(A, n) == 0) {
+            System.out.println("La matriz es no invertible");
+        } else {
+            for (int k = 0; k < n - 1; ++k) {
+                if (A[k][k] == 0) {
+                    int j = k + 1;
+                    while (j < n && A[j++][k] == 0) ;
+                    if (j < n) {
+                        for (int l = k; l < n; ++l) {
+                            A[k][l] += A[j][l];
+                        }
+                        b[k] += b[j];
+                    }
+                }
+                for (int i = k + 1; i < n; ++i) {
+                    if (A[i][k] != 0) {
+                        double multiplicador = A[i][k] / A[k][k];
+                        for (int l = k; l < n; ++l) {
+                            A[i][l] -= multiplicador * A[k][l];
+                        }
+                        b[i] -= multiplicador * b[k];
+                    }
+                }
+            }
+        }
+        return new Utilities.Par<>(A, b);
     }
 }
